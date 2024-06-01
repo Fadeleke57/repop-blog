5dea5d73f22fba003a42ff70371d28e5618f87f9
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-06-01 02:04:47 -0400

    permissions

--- api/middleware.js
+++ api/middleware.js
@@ -5,7 +5,18 @@ const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 
 const configureMiddleware = (app) => {
-  app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+  const allowedOrigins = ['http://localhost:3000', 'https://repop-blog.vercel.app'];
+
+  app.use(cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }}));
+    
   app.use(express.json());
   app.use(cookieParser());
   app.use('/uploads', express.static(__dirname + '/uploads'));
