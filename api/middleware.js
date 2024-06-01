0f08d206d6918bac72549f57e72b28f39bf91980
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-06-01 01:38:38 -0400

    allowing local dev from cors

--- api/middleware.js
+++ api/middleware.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 
 const configureMiddleware = (app) => {
-  app.use(cors({ credentials: true, origin: 'https://repop-blog-server.onrender.com/' }));
+  app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
   app.use(express.json());
   app.use(cookieParser());
   app.use('/uploads', express.static(__dirname + '/uploads'));
