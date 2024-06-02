2bc97ea3a324122922177e3b0dfb64e5ee8d4e9d
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-06-02 13:44:21 -0400

    PostPage styling

--- api/middleware.js
+++ api/middleware.js
@@ -5,9 +5,19 @@ const cookieParser = require('cookie-parser');
 //const uploadMiddleware = multer({ dest: 'uploads/' });
 
 const configureMiddleware = (app) => {
-  const allowedOrigins = ['http://localhost:3000', 'https://repop-blog.vercel.app'];
+  const allowedOrigins = ['http://localhost:3000', 'https://repop-blog.vercel.app', 'https://repop-blog-server.onrender.com'];
 
-  app.use(cors({credentials: true, origin: true}));
+  app.use(cors({
+    credentials: true,
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
     
   app.use(express.json());
   app.use(cookieParser());
